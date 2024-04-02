42331969d7bf49a6eb0e62228e3608f8aa65dea7
lmwilliams27 <143618390+lmwilliams27@users.noreply.github.com>
2024-04-02 13:37:17 +0000

    freakbob

---
+++ style.js
@@ -0,0 +1,21 @@
+// let screenButton=document.querySelector(".screenButton");
+let result1=document.querySelector(".result1");
+let submitButton=document.querySelector(".lipstickButton");
+let freaky=document.querySelector(".freaky");
+let lipstick= document.querySelector(".lipstick");
+let freakybob=document.querySelector(".freakybob");
+let lipstickInput=document.querySelector(".lipstick").value;
+let freakyInput= document.querySelector(".freaky").value;
+console.log("baby");
+
+
+submitButton.addEventListener("click", function(){
+let lipstickInput=document.querySelector(".lipstick").value;
+
+if (freakyInput==="More than 5"&& lipstickInput==="Coral Blue No.5"){
+    document.getElementById("freakybob").src ="freakybob.jpg"; 
+}
+if(lipstickInput==="Red or something"){
+
+}
+});
